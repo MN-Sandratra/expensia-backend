e62bdb97cec28a57fc6ceef266741afef98c625c
MB-Sandratra <mn.sandratra@gmail.com>
2025-03-23 00:12:16 +0100

    first commit

---
+++ src/main/java/com/expensia/expensia_backend/ExpensiaApplication.java
@@ -0,0 +1,13 @@
+package com.expensia.expensia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/expensia/expensia_backend/ExpensiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensia.expensia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
